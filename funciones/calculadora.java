fbbd7d3575cbebe936984e8a3707ba29e59c71ea
JonaRiosBrambila <95506962+JonaRiosBrambila@users.noreply.github.com>
2022-02-02 17:42:25 -0600

    Add files via upload

---
+++ funciones/calculadora.java
@@ -0,0 +1,36 @@
+package funciones;
+
+import java.util.Scanner;
+
+public class calculadora {
+	
+	static double suma(double a,double b) {
+		return a+b;
+	}
+	static double resta(double a,double b) {
+		return a-b;
+	}
+	static double multiplicacion(double a,double b) {
+		return a*b;
+	}
+	static double division(double a,double b) {
+		return a/b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner s1= new Scanner(System.in);
+      System.out.println("Introduce el primer numero;");
+      double x = s1.nextDouble();
+      
+      Scanner s2= new Scanner(System.in);
+      System.out.println("Introduce el primer numero;");
+      double y = s2.nextDouble();
+      
+      System.out.println("La suma es;"+suma(x,y));
+      System.out.println("La resta es;"+resta(x,y));
+      System.out.println("La multiplicacion es;"+multiplicacion(x,y));
+      System.out.println("La division es;"+division(x,y));
+	}
+
+}
---
+++ funciones/funcionReturn.java
@@ -0,0 +1,10 @@
+package funciones;
+
+public class funcionReturn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
